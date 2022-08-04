0392d8a886bf59807ac62c3bcf56c4c98640ff5e
abhidcoder <74507289+abhidcoder@users.noreply.github.com>
2022-08-04 22:35:45 +0530

    Update firebaseconfig.js

--- src/Firebase/firebaseconfig.js
+++ src/Firebase/firebaseconfig.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAkaVjh0r7yyLWikIj1oIJ4nx1EOqkkNHc",
-  authDomain: "auth-90d5f.firebaseapp.com",
-  projectId: "auth-90d5f",
-  storageBucket: "auth-90d5f.appspot.com",
-  messagingSenderId: "1044539955305",
-  appId: "1:1044539955305:web:d85ed748da8ae3843caaf8",
-  measurementId: "G-5FTD86ER6H"
+  apiKey: "AIzaSyB11z6kUVcJTUzt4hYmoozHgAiinUP62HM",
+  authDomain: "abhishek-s-your-aid.firebaseapp.com",
+  projectId: "abhishek-s-your-aid",
+  storageBucket: "abhishek-s-your-aid.appspot.com",
+  messagingSenderId: "426957616019",
+  appId: "1:426957616019:web:219fe33b0e57ca8459ae66",
+  measurementId: "G-P2CPG52CV4"
 };
 
 // Initialize Firebase
